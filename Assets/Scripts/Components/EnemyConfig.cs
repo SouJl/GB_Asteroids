84eadbe846d5d4cfd13c264633674c273b82fb88
SouJl <meln.alex97@gmail.com>
2022-10-24 11:22:14 +0700

    Create ScriptableObject EnemyConfig
    создание ScriptableObject параметров врагов и создание 2 объекта на его основе

---
+++ Assets/Scripts/Components/EnemyConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GB_Asteroids
+{ 
+    [CreateAssetMenu(fileName = "EnemyConfig", menuName = "ScriptableObjects/EnemyScriptableObject")]
+    public class EnemyConfig: ScriptableObject
+    {
+        public string Name;
+        public EnemyType Type;
+
+        public float MaxHealth;
+        public float Damage;
+        public float Points;
+
+        public GameObject Prefab;
+    }
+}

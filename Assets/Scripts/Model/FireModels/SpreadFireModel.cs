3e9cad3d2f796d168483f9dd1fb069ccd386935b
Александр Мельников <meln.alex97@gmail.com>
2022-11-07 21:16:18 +0700

    Spead Fire Realisation

--- Assets/Scripts/Model/FireModels/SpreadFireModel.cs
+++ Assets/Scripts/Model/FireModels/SpreadFireModel.cs
@@ -10,9 +10,29 @@ namespace GB_Asteroids.FireModels
 
         }
 
-        public override void Fire()
+
+        public override void Fire() 
+        { 
+            var bulletLeft = MakeProjectile(); 
+            var bulletCentr = MakeProjectile(); 
+            var bulletRight = MakeProjectile(); 
+ 
+            bulletLeft.Rigidbody.AddForce(FireAngle(10) * Force, ForceMode.Force); 
+            bulletCentr.Rigidbody.AddForce(FireAngle(0)* Force, ForceMode.Force); 
+            bulletRight.Rigidbody.AddForce(FireAngle(-10) * Force, ForceMode.Force); 
+        } 
+ 
+        private BulletView MakeProjectile() 
+        { 
+            var bullet = ServiceLocator.Resolve<IViewBuilderService>().Instantiate<Transform>(BulletConfig); 
+            bullet.transform.position = FirePoint.position; 
+ 
+            return bullet.gameObject.AddComponent<BulletView>(); 
+        } 
+ 
+        private Vector3 FireAngle(float angle) 
         {
-            
+            return Quaternion.AngleAxis(angle, Vector3.back) * FirePoint.up;          
         }
     }
 }
